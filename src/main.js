44ac7347c95461fe9e0b13680b28432678cb006f
NanXiangXD <zjm050622@foxmail.com>
2026-09-22 09:59:53 +0800

    feat: add van-search on home page and register Search component

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-import { Tabbar, TabbarItem } from 'vant'
+import { Tabbar, TabbarItem, Search } from 'vant'
 import 'vant/lib/index.css'
 
-createApp(App).use(router).use(Tabbar).use(TabbarItem).mount('#app')
+createApp(App).use(router).use(Tabbar).use(TabbarItem).use(Search).mount('#app')
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,25 @@
 <template>
-  <div>Home</div>
+  <van-search
+    v-model="searchValue"
+    shape="round"
+    show-cancel
+    placeholder="请输入搜索关键词"
+    @search="onSearch"
+    @cancel="onCancel"
+  />
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { showToast } from 'vant'
+
+const searchValue = ref('')
+
+const onSearch = () => {
+  showToast(`搜索：${searchValue.value}`)
+}
+
+const onCancel = () => {
+  showToast('已取消搜索')
+}
+</script>
